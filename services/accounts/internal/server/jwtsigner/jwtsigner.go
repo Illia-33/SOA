14252e442a52ffe66245a0e68e2f1f20ec3fc570
illia33 <ilya.zavalov@gmail.com>
2025-08-20 16:43:29 +0300

    Replace MapClaims with custom claims

--- services/accounts/internal/server/jwtsigner/jwtsigner.go
+++ services/accounts/internal/server/jwtsigner/jwtsigner.go
@@ -2,7 +2,6 @@ package jwtsigner
 
 import (
 	"crypto/ed25519"
-	"encoding/json"
 	"time"
 
 	"soa-socialnetwork/internal/soajwt"
@@ -26,46 +25,26 @@ func New(privateKey ed25519.PrivateKey) (Signer, error) {
 	}, nil
 }
 
-func (j *Signer) generateClaims(data PersonalData, ttl time.Duration) (jwt.Claims, error) {
+func (j *Signer) Sign(data PersonalData, ttl time.Duration) (string, error) {
 	jwtUuid, err := uuid.NewV7()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	now := time.Now()
 
 	token := soajwt.Token{
+		Issuer:    "accounts-service",
+		Subject:   data.ProfileId,
+		Audience:  []string{"user"},
 		ExpiresAt: now.Add(ttl),
+		NotBefore: now,
 		IssuedAt:  now,
 		JwtId:     jwtUuid.String(),
 		AccountId: data.AccountId,
-		ProfileId: data.ProfileId,
 	}
 
-	// TODO optimize soajwt-to-map conversion
-
-	asJson, err := json.Marshal(token)
-	if err != nil {
-		return nil, err
-	}
-
-	var claims map[string]any
-	err = json.Unmarshal(asJson, &claims)
-	if err != nil {
-		return nil, err
-	}
-
-	return jwt.MapClaims(claims), nil
-}
-
-func (j *Signer) Sign(data PersonalData, ttl time.Duration) (string, error) {
-	claims, err := j.generateClaims(data, ttl)
-	if err != nil {
-		return "", err
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodEdDSA, claims)
-	tokenStr, err := token.SignedString(j.privateKey)
+	tokenStr, err := jwt.NewWithClaims(jwt.SigningMethodEdDSA, &token).SignedString(j.privateKey)
 	if err != nil {
 		return "", err
 	}
