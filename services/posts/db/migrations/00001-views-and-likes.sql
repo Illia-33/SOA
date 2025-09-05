5dff9a039cac3bfd67ab0a6327736f51bc62d9e6
illia33 <ilya.zavalov@gmail.com>
2025-09-05 17:13:09 +0300

    Unite migrations from the same PR

--- services/posts/db/migrations/00001-views-and-likes.sql
+++ services/posts/db/migrations/00001-views-and-likes.sql
@@ -1,6 +1,9 @@
+ALTER TABLE posts ADD COLUMN views_count INTEGER NOT NULL DEFAULT 0;
+
 CREATE TABLE IF NOT EXISTS likes (
     post_id INTEGER NOT NULL,
     author_account_id INTEGER NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     PRIMARY KEY (post_id, author_account_id)
 );
+
