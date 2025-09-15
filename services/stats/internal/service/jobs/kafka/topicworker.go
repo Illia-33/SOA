368bd4f5203842789d7599d10a0a87784b80e91b
illia33 <ilya.zavalov@gmail.com>
2025-09-15 16:07:00 +0300

    Simplify topic worker

--- services/stats/internal/service/jobs/kafka/topicworker.go
+++ services/stats/internal/service/jobs/kafka/topicworker.go
@@ -14,7 +14,6 @@ type batchProcessor[MsgType any] func(context.Context, messageBatch[MsgType]) er
 
 type topicWorker[MsgType any] struct {
 	consumer     kafka.Consumer[MsgType]
-	messages     chan kafka.Message[MsgType]
 	processBatch batchProcessor[MsgType]
 }
 
@@ -35,87 +34,84 @@ func newTopicProcessor[MsgType any](
 }
 
 func (r *topicWorker[MsgType]) start(ctx context.Context) {
-	const MSG_CAPACITY = 100
-	r.messages = make(chan kafka.Message[MsgType], MSG_CAPACITY)
-	r.startConsume(ctx)
-	r.startWork(ctx)
-}
-
-func (r *topicWorker[MsgType]) startConsume(ctx context.Context) {
-	go func() {
-		for {
-			msg, err := r.consumer.FetchMessage(ctx)
-			if err != nil {
-				if errors.Is(err, io.EOF) {
-					break
-				}
-
-				log.Printf("error occured while fetching %T message from kafka: %v", msg, err)
-				time.Sleep(time.Second)
-				continue
-			}
-
-			r.messages <- msg
-		}
-
-		close(r.messages)
-		r.close()
-	}()
-}
-
-func (r *topicWorker[MsgType]) startWork(ctx context.Context) {
 	go func() {
 		const BATCH_CAPACITY = 10
 		batch := make(messageBatch[MsgType], 0, BATCH_CAPACITY)
+		batchProcessed := false
+		messages := make(chan kafka.Message[MsgType], BATCH_CAPACITY)
 
-		processBatch := func() error {
+		go func() {
+			for {
+				msg, err := r.consumer.FetchMessage(ctx)
+				if err != nil {
+					if errors.Is(err, io.EOF) {
+						break
+					}
+
+					log.Printf("error occured while fetching %T message from kafka: %v", msg, err)
+					time.Sleep(500 * time.Millisecond)
+					continue
+				}
+				messages <- msg
+			}
+
+			close(messages)
+		}()
+
+		tryProcessBatch := func() error {
 			if len(batch) == 0 {
 				return nil
 			}
-			err := r.processBatch(ctx, batch)
+
+			if !batchProcessed {
+				err := r.processBatch(ctx, batch)
+				if err != nil {
+					log.Printf("error occured while processing %T batch: %v", batch, err)
+					return err
+				}
+				batchProcessed = true
+			}
+
+			err := r.consumer.CommitMessages(ctx, batch...)
 			if err != nil {
+				log.Printf("error occured while committing %T batch: %v", batch, err)
 				return err
 			}
 
 			batch = batch[:0]
+			batchProcessed = false
 			return nil
-		}
-
-		processBatchWithLogging := func() error {
-			err := processBatch()
-			if err != nil {
-				log.Printf("error occured while processing %T batch: %v", batch, err)
-			}
-
-			return err
 		}
 
 		done := false
 		for !done {
-			for len(batch) >= BATCH_CAPACITY {
-				err := processBatchWithLogging()
+			if len(batch) >= BATCH_CAPACITY {
+				err := tryProcessBatch()
 				if err != nil {
 					time.Sleep(500 * time.Millisecond)
 				}
+				continue
 			}
 
 			select {
-			case msg, ok := <-r.messages:
+			case msg, ok := <-messages:
 				{
 					if !ok {
 						done = true
 						break
 					}
-
 					batch = append(batch, msg)
 				}
 
 			case <-time.NewTimer(500 * time.Millisecond).C:
 				{
-					processBatchWithLogging()
+					tryProcessBatch()
 				}
 			}
+
 		}
+
+		r.close()
 	}()
 }
 
