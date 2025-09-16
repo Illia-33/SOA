24114912db951e5526f7193593e690098e4fc7e4
illia33 <ilya.zavalov@gmail.com>
2025-09-16 16:37:58 +0300

    Add tests

--- tests/e2e/stats_test.go
+++ tests/e2e/stats_test.go
@@ -22,6 +22,27 @@ func getMetricOk(t *testing.T, postId int, metric string) int {
 	return int(responseBodyToMap(t, resp)["count"].(float64))
 }
 
+func tryGetMetricDynamics(t *testing.T, postId int, metric string) *http.Response {
+	body := map[string]any{
+		"metric": metric,
+	}
+	return makeRequest(t, http.MethodGet, fmt.Sprintf("/post/%d/metric_dynamics", postId), body, "")
+}
+
+func getMetricDynamicsOk(t *testing.T, postId int, metric string) []map[string]any {
+	resp := tryGetMetricDynamics(t, postId, metric)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+
+	dynamicsAny := (responseBodyToMap(t, resp)["dynamics"]).([]any)
+
+	dynamics := make([]map[string]any, len(dynamicsAny))
+	for i := range dynamicsAny {
+		dynamics[i] = dynamicsAny[i].(map[string]any)
+	}
+
+	return dynamics
+}
+
 func TestGetViewCount(t *testing.T) {
 	id := registerUserOk(t, map[string]any{
 		"login":        "get_view_count",
@@ -125,4 +146,112 @@ func TestGetCommentCount(t *testing.T) {
 
 	commentCount := getMetricOk(t, postId, "comment_count")
 	require.Equal(t, COMMENT_COUNT, commentCount)
+}
+
+func TestGetViewCountDynamics(t *testing.T) {
+	id := registerUserOk(t, map[string]any{
+		"login":        "get_view_count_dynamics",
+		"password":     "testpasswd",
+		"email":        "get_view_count_dynamics@yahoo.com",
+		"phone_number": "+79250000025",
+		"name":         "Get",
+		"surname":      "ViewCountDynamics",
+	})
+	token := authenticateOk(t, map[string]any{
+		"login":    "get_view_count_dynamics",
+		"password": "testpasswd",
+	})
+
+	post := map[string]any{
+		"text": "test post with views",
+	}
+	postId := createPostOk(t, id, post, jwtAuth(token))
+
+	const VIEW_COUNT = 5
+	for range VIEW_COUNT {
+		newViewOk(t, postId, jwtAuth(token))
+	}
+
+	time.Sleep(10 * time.Second)
+
+	dynamics := getMetricDynamicsOk(t, postId, "view_count")
+	require.Equal(t, 1, len(dynamics))
+	require.Equal(t, VIEW_COUNT, int(dynamics[0]["count"].(float64)))
+}
+
+func TestGetLikeCountDynamics(t *testing.T) {
+	id := registerUserOk(t, map[string]any{
+		"login":        "get_like_count_dynamics",
+		"password":     "testpasswd",
+		"email":        "get_like_count_dynamics@yahoo.com",
+		"phone_number": "+79250000026",
+		"name":         "Get",
+		"surname":      "ViewCountDynamics",
+	})
+	token := authenticateOk(t, map[string]any{
+		"login":    "get_like_count_dynamics",
+		"password": "testpasswd",
+	})
+
+	post := map[string]any{
+		"text": "test post with likes",
+	}
+	postId := createPostOk(t, id, post, jwtAuth(token))
+
+	const LIKE_COUNT = 5
+	for i := range LIKE_COUNT {
+		registerUserOk(t, map[string]any{
+			"login":        fmt.Sprintf("get_like_count_dynamics_%d", i),
+			"password":     "testpasswd",
+			"email":        fmt.Sprintf("get_like_count_dynamics_%d@yahoo.com", i),
+			"phone_number": fmt.Sprintf("+7927000000%d", i),
+			"name":         "Get",
+			"surname":      fmt.Sprintf("LikeCountDynamics%d", i),
+		})
+		likerToken := authenticateOk(t, map[string]any{
+			"login":    fmt.Sprintf("get_like_count_dynamics_%d", i),
+			"password": "testpasswd",
+		})
+		newLikeOk(t, postId, jwtAuth(likerToken))
+	}
+
+	time.Sleep(10 * time.Second)
+
+	dynamics := getMetricDynamicsOk(t, postId, "like_count")
+	require.Equal(t, 1, len(dynamics))
+	require.Equal(t, LIKE_COUNT, int(dynamics[0]["count"].(float64)))
+}
+
+func TestGetCommentCountDynamics(t *testing.T) {
+	id := registerUserOk(t, map[string]any{
+		"login":        "get_comment_count_dynamics",
+		"password":     "testpasswd",
+		"email":        "get_comment_count_dynamics@yahoo.com",
+		"phone_number": "+79250000027",
+		"name":         "Get",
+		"surname":      "CommentCountDynamics",
+	})
+	token := authenticateOk(t, map[string]any{
+		"login":    "get_comment_count_dynamics",
+		"password": "testpasswd",
+	})
+
+	post := map[string]any{
+		"text": "test post with comments",
+	}
+	postId := createPostOk(t, id, post, jwtAuth(token))
+
+	const COMMENT_COUNT = 5
+	for i := range COMMENT_COUNT {
+		comment := map[string]any{
+			"content": fmt.Sprintf("comment #%d", i),
+		}
+		newCommentOk(t, postId, comment, jwtAuth(token))
+	}
+
+	time.Sleep(10 * time.Second)
+
+	dynamics := getMetricDynamicsOk(t, postId, "comment_count")
+	require.Equal(t, 1, len(dynamics))
+	require.Equal(t, COMMENT_COUNT, int(dynamics[0]["count"].(float64)))
 }
