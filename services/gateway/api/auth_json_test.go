ab363c78c29ec938b9001774373f55f01f381dd5
illia33 <ilya.zavalov@gmail.com>
2025-09-24 12:52:11 +0300

    Add tests

---
+++ services/gateway/api/auth_json_test.go
@@ -0,0 +1,54 @@
+package api
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func unmarshal[T any](s string) (T, error) {
+	var unmarshalled T
+	err := json.Unmarshal([]byte(s), &unmarshalled)
+	return unmarshalled, err
+}
+
+func TestAuthSimple(t *testing.T) {
+	rawJson := `
+{
+	"login": "some_login",
+	"password": "some_passwd"
+}
+	`
+
+	req, err := unmarshal[AuthenticateRequest](rawJson)
+
+	require.NoError(t, err, "valid json unmarshalling failed")
+	assert.Equal(t, "some_login", req.Login)
+	assert.Equal(t, "some_passwd", req.Password)
+}
+
+func TestAuthTooMuchUserId(t *testing.T) {
+	rawJson := `
+{
+	"login": "some_login",
+	"email": "some_email@yahoo.com",
+	"password": "some_passwd"
+}
+	`
+
+	_, err := unmarshal[AuthenticateRequest](rawJson)
+	require.ErrorAs(t, err, &ErrorTooMuchUserId{})
+}
+
+func TestAuthNoUserId(t *testing.T) {
+	rawJson := `
+{
+	"password": "some_passwd"
+}
+	`
+
+	_, err := unmarshal[AuthenticateRequest](rawJson)
+	require.ErrorAs(t, err, &ErrorNoUserId{})
+}
